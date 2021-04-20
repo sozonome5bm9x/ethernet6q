9371cce3c78b8917db81d5cb1fe0acb75236740f
sozonome5bm9x <uqujwkwo@163.com>
2021-04-20 18:33:41 +0800

    Tabs Setup EP3 VID 04

---
+++ app/src/main/java/com/example/forsalev2/AccountFragment.java
@@ -0,0 +1,22 @@
+package com.example.forsalev2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AccountFragment extends Fragment {
+
+    private static final String TAG = "AccountFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        return view;
+    }
+}
